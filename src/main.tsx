06fbe688114ccf8d1576a00ffc2c3605f43ad365
Ameer Hamza <ameerhamza@salsoft.net>
2026-07-07 21:27:32 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-// import App from "./App";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    {/* <App /> */}
-    <h1>This is funtology</h1>
+    <App />
   </Provider>
 );
